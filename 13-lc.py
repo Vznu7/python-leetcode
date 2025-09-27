efa4e01bfb7514cfec0323f1f4408477a98946b7
vznu7 <vishnuvishnu49373@gmail.com>
2025-09-27 20:57:24 +0530

    added another problem

--- 13-lc.py
+++ 13-lc.py
@@ -11,15 +11,15 @@ class Solution(object):
             'M': 1000
         }
 
-        total = 0
+        t = 0
         prev = 0
 
         for ch in reversed(s): 
             curr = roman[ch]
             if curr < prev:
-                total -= curr
+                t -= curr
             else:
-                total += curr
+                t += curr
             prev = curr
 
-        return total
+        return t
