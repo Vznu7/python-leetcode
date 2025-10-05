efe2e8b01575afb7e4f306a5d181d95553b4183a
vznu7 <vishnuvishnu49373@gmail.com>
2025-10-05 22:06:44 +0530

    updated

--- 242-lc.py
+++ 242-lc.py
@@ -1,4 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+#another approach
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+
         
