3a280d32596802cb7154ef4b229dad1893261676
VISHNU <162817538+Vznu7@users.noreply.github.com>
2026-01-01 19:26:46 +0530

    Update 283-move-zeros.py

--- algo_master-300/01-Arrays/283-move-zeros.py
+++ algo_master-300/01-Arrays/283-move-zeros.py
@@ -1,8 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+       
         l=0
         for r in range(len(nums)):
             if nums[r]:
